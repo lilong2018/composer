fd133c7a1c0177cea997c07266890bc8f6f5c624
lilong <571914591@qq.com>
2019-08-23 14:51:17 +0800

    composer示例

---
+++ lilong/Say.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 编辑器：PhpStorm
+ * 编写者: 莉龙
+ * 编写时间: 2019/8/23 14:42
+ */
+
+namespace lilong;
+
+class Say {
+    /**
+     * love方法
+     * @return string 返回要说的话
+     */
+    public static function love() {
+        return "5201314@YangLi";
+    }
+}
+
